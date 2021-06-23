216e39806b9f6d33996ddc1ff67b2d928e57bf9e
Shachidanand Pandit <shachi.soft@gmail.com>
2021-06-23 18:55:33 +0545

    Added VelocityX and updated Detail Page

--- lib/pages/details_page.dart
+++ lib/pages/details_page.dart
@@ -1,5 +1,7 @@
+import 'package:big_app/models/themes.dart';
 import 'package:flutter/material.dart';
 import 'package:big_app/models/catalog.dart';
+import 'package:velocity_x/velocity_x.dart';
 
 class HomeDetailPage extends StatelessWidget {
   final Item catalog;
@@ -11,7 +13,7 @@ class HomeDetailPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: MyTheme.backColor,
       appBar: AppBar(
         title: Image.asset(
           "assets/images/logo.png",
@@ -20,35 +22,96 @@ class HomeDetailPage extends StatelessWidget {
       ),
       body: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.only(top: 16.0),
+          padding: const EdgeInsets.only(top: 0.0),
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.start,
             children: [
-              Center(
-                child: Hero(
-                  tag: Key(
-                    catalog.id.toString(),
-                  ),
-                  child: Image.network(
-                    catalog.image,
-                    width: 300.0,
+              //////////////////////// IMAGE
+              Container(
+                decoration: const BoxDecoration(),
+                child: Center(
+                  child: Hero(
+                    tag: Key(
+                      catalog.id.toString(),
+                    ),
+                    child: Image.network(
+                      catalog.image,
+                      width: 300.0,
+                    ),
                   ),
                 ),
               ),
-              const SizedBox(
-                height: 10,
-              ),
+              // const SizedBox(
+              //   height: 10,
+              // ),
+              //////////////// PRICE AND NAME
               Expanded(
-                child: Container(
-                  color: Colors.grey,
-                  width: MediaQuery.of(context).size.width,
-                  child: Column(
-                    children: [
-                      Text(catalog.name),
-                    ],
+                child: VxArc(
+                  height: 0,
+                  edge: VxEdge.TOP,
+                  arcType: VxArcType.CONVEY,
+                  child: Container(
+                    color: Colors.white,
+                    width: context.screenWidth,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        /////////////// item price
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            "रु. ${catalog.price}"
+                                .toString()
+                                .text
+                                .bold
+                                .xl3
+                                .color(MyTheme.orangeColor)
+                                .make(),
+                            SizedBox(width: 10),
+                            (catalog.discount > 0)
+                                ? "रु. ${catalog.mrp}"
+                                    .toString()
+                                    .text
+                                    .lineThrough
+                                    .xl
+                                    .color(MyTheme.blackColor)
+                                    .make()
+                                : "".text.make(),
+                            SizedBox(width: 10),
+                            (catalog.discount > 0)
+                                ? "${catalog.discount} % off"
+                                    .toString()
+                                    .text
+                                    .bold
+                                    .xl
+                                    .color(Colors.green)
+                                    .make()
+                                : "".text.make(),
+                          ],
+                        ).px16(),
+
+                        ///////////////////// item name
+                        catalog.name.text.xl2.capitalize.make().px16(),
+                        ///////// sizedbox
+                        SizedBox(
+                          height: 10,
+                          child: Container(
+                            color: MyTheme.backColor,
+                          ),
+                        ),
+                        Container(
+                          width: context.screenWidth,
+                          child: "Discription :"
+                              .text
+                              .xl
+                              .color(MyTheme.blackColor)
+                              .make(),
+                        ).p16(),
+                      ],
+                    ).py16(),
                   ),
                 ),
-              )
+              ),
             ],
           ),
         ),
